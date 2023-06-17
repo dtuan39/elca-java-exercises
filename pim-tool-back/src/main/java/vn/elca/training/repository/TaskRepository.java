af7b7a29c08125e3d002ca2ce4f2ed658407d7ae
thomas.dang <thongdh3401@gmail.com>
2023-06-17 20:47:22 +0700

    unnecessary annotations

--- pim-tool-back/src/main/java/vn/elca/training/repository/TaskRepository.java
+++ pim-tool-back/src/main/java/vn/elca/training/repository/TaskRepository.java
@@ -2,7 +2,6 @@ package vn.elca.training.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.stereotype.Repository;
 import vn.elca.training.model.entity.Task;
 import vn.elca.training.repository.custom.TaskRepositoryCustom;
 
@@ -10,7 +9,6 @@ import vn.elca.training.repository.custom.TaskRepositoryCustom;
  * @author vlp
  *
  */
-@Repository
 public interface TaskRepository extends JpaRepository<Task, Long>, QuerydslPredicateExecutor<Task>, TaskRepositoryCustom {
 
 }
