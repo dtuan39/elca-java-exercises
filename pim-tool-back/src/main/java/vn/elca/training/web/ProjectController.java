3fdc4c85528ce3c07407bd1f5e1d8f8151081233
thomas.dang <thongdh3401@gmail.com>
2023-06-17 20:41:40 +0700

    Complete the query-by-name on “search” feature

--- pim-tool-back/src/main/java/vn/elca/training/web/ProjectController.java
+++ pim-tool-back/src/main/java/vn/elca/training/web/ProjectController.java
@@ -3,11 +3,13 @@ package vn.elca.training.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import vn.elca.training.model.dto.ProjectDto;
 import vn.elca.training.service.ProjectService;
 import vn.elca.training.util.ApplicationMapper;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,9 +25,14 @@ public class ProjectController extends AbstractApplicationController {
     private ProjectService projectService;
 
     @GetMapping("/search")
-    public List<ProjectDto> search() {
+    public List<ProjectDto> search(@RequestParam String keyword) {
+        if (keyword.isBlank()) {
+            return new ArrayList<>();
+        }
         return projectService.findAll()
                 .stream()
+                .filter(product -> product.getName().trim().toLowerCase()
+                        .contains(keyword.trim().toLowerCase()))
                 .map(mapper::projectToProjectDto)
                 .collect(Collectors.toList());
     }
