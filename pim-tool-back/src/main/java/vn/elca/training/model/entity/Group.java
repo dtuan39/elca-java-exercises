e620c6448f1c701bdb7543ab07ddd857e2f7c265
thomas.dang <thongdh3401@gmail.com>
2023-06-24 01:29:08 +0700

    configure the respective entities as required

--- pim-tool-back/src/main/java/vn/elca/training/model/entity/Group.java
+++ pim-tool-back/src/main/java/vn/elca/training/model/entity/Group.java
@@ -1,70 +1,27 @@
 package vn.elca.training.model.entity;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 /**
  * @author thomas.dang
  */
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
-@Table(name = "\"Group\"")
+@Table(name = "\"GROUP\"")
 public class Group {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
     private Long id;
 
-    @OneToMany
-    @JoinColumn(name = "group_id")
-    private Set<Project> projects = new LinkedHashSet<>();
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "GROUP_LEADER_ID", nullable = false)
+    private Employee groupLeader;
 
-    @OneToOne
-    @JoinColumn(name = "leader_id")
-    private User leader;
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((projects == null) ? 0 : projects.hashCode());
-        result = prime * result + ((leader == null) ? 0 : leader.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Group other = (Group) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        if (projects == null) {
-            if (other.projects != null)
-                return false;
-        } else if (!projects.equals(other.projects))
-            return false;
-        if (leader == null) {
-            if (other.leader != null)
-                return false;
-        } else if (!leader.equals(other.leader))
-            return false;
-        return true;
-    }
-
-    
+    @Column(name = "VERSION", nullable = false)
+    private Integer version;
 }
--- pim-tool-back/src/main/java/vn/elca/training/model/entity/Project.java
+++ pim-tool-back/src/main/java/vn/elca/training/model/entity/Project.java
@@ -1,120 +1,46 @@
 package vn.elca.training.model.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
 
 /**
- * @author vlp
+ * @author thomas.dang
  */
 @Entity
-@Setter
 @Getter
-@NoArgsConstructor
+@Setter
+@Table(name = "PROJECT")
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "GROUP_ID", nullable = false)
+    private Group group;
+
+    @Column(name = "PROJECT_NUMBER", nullable = false)
+    private Integer projectNumber;
+
+    @Column(name = "NAME", nullable = false, length = 50)
     private String name;
 
-    @Column
-    private LocalDate finishingDate;
-
-    @Column
+    @Column(name = "CUSTOMER", nullable = false, length = 50)
     private String customer;
 
-    @Column(nullable = true)
-    private boolean activated;
+    @Column(name = "STATUS", nullable = false, length = 3)
+    private String status;
 
-    @OneToOne
-    @JoinColumn(name = "leader_id")
-    private User leader;
+    @Column(name = "START_DATE", nullable = false)
+    private LocalDate startDate;
 
-    @OneToMany
-    @JoinColumn(name = "project_id")
-    // manage who worked in this project
-    private Set<User> users = new LinkedHashSet<>();
+    @Column(name = "END_DATE")
+    private LocalDate endDate;
 
-    @OneToMany(mappedBy = "project", fetch = FetchType.LAZY)
-    private Set<Task> tasks = new HashSet<>();
-
-    public Project(String name, LocalDate finishingDate) {
-        this.name = name;
-        this.finishingDate = finishingDate;
-    }
-
-    public Project(Long id, String name, LocalDate finishingDate) {
-        this.id = id;
-        this.name = name;
-        this.finishingDate = finishingDate;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + ((finishingDate == null) ? 0 : finishingDate.hashCode());
-        result = prime * result + ((customer == null) ? 0 : customer.hashCode());
-        result = prime * result + (activated ? 1231 : 1237);
-        result = prime * result + ((leader == null) ? 0 : leader.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Project other = (Project) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        if (finishingDate == null) {
-            if (other.finishingDate != null)
-                return false;
-        } else if (!finishingDate.equals(other.finishingDate))
-            return false;
-        if (customer == null) {
-            if (other.customer != null)
-                return false;
-        } else if (!customer.equals(other.customer))
-            return false;
-        if (activated != other.activated)
-            return false;
-        if (leader == null) {
-            if (other.leader != null)
-                return false;
-        } else if (!leader.equals(other.leader))
-            return false;
-        return true;
-    }
-
+    @Column(name = "VERSION", nullable = false)
+    private Integer version;
 }
