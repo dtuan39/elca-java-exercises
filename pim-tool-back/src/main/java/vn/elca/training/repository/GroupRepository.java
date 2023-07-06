690909a4ec1ae33f7d402a250203eef1aefdaae6
thomas.dang <thongdh3401@gmail.com>
2023-07-06 09:48:58 +0700

    implement load all groups endpoint

--- pim-tool-back/src/main/java/vn/elca/training/repository/GroupRepository.java
+++ pim-tool-back/src/main/java/vn/elca/training/repository/GroupRepository.java
@@ -2,7 +2,9 @@ package vn.elca.training.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import vn.elca.training.model.entity.Group;
 
-public interface GroupRepository extends JpaRepository<Group, Long> {
+public interface GroupRepository extends JpaRepository<Group, Long> , QuerydslPredicateExecutor<Group> {
+
 }
