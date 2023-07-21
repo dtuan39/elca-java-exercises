becdb930f7527335f420e44f5cfec8dc3547c5b1
thomas.dang <thongdh3401@gmail.com>
2023-07-21 22:57:50 +0700

    remove unused

--- pim-tool-back/src/main/java/vn/elca/training/repository/ProjectRepository.java
+++ pim-tool-back/src/main/java/vn/elca/training/repository/ProjectRepository.java
@@ -2,20 +2,13 @@ package vn.elca.training.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.stereotype.Repository;
 import vn.elca.training.model.entity.Project;
 import vn.elca.training.repository.custom.ProjectRepositoryCustom;
-
-import java.util.Optional;
 
 /**
  * @author vlp
  * @author thomas.dang
  */
-@Repository
 public interface ProjectRepository extends JpaRepository<Project, Long>,
         QuerydslPredicateExecutor<Project>, ProjectRepositoryCustom {
-    Optional<Project> findByProjectNumber(Integer projectNumber);
-
-    void deleteByProjectNumber(Integer projectNumber);
 }
