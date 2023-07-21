52ea67bb979c6d0cbfff1f174847c7d171314631
thomas.dang <thongdh3401@gmail.com>
2023-07-21 22:56:37 +0700

    optimize for detailed information

--- pim-tool-back/src/main/java/vn/elca/training/model/exception/ProjectNotFoundException.java
+++ pim-tool-back/src/main/java/vn/elca/training/model/exception/ProjectNotFoundException.java
@@ -4,4 +4,12 @@ public class ProjectNotFoundException extends Exception{
     public ProjectNotFoundException() {
         super("Project not found");
     }
+
+    public ProjectNotFoundException(Long id) {
+        super(String.format("Project with id %d not found", id));
+    }
+
+    public ProjectNotFoundException(Integer projectNumber) {
+        super(String.format("Project with number %d not found", projectNumber));
+    }
 }
