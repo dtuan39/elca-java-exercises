312cf31e4aa5ddffaf4ccabe1f1f8f7865239111
thomas.dang <thongdh3401@gmail.com>
2023-07-27 20:43:53 +0700

    replace h2 with mysql

--- pim-tool-back/src/main/java/vn/elca/training/ApplicationWebConfig.java
+++ pim-tool-back/src/main/java/vn/elca/training/ApplicationWebConfig.java
@@ -1,17 +1,14 @@
 package vn.elca.training;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import vn.elca.training.model.mapping.GroupMapper;
 import vn.elca.training.model.mapping.ProjectMapper;
 import vn.elca.training.service.ProjectService;
-import vn.elca.training.util.ApplicationMapper;
 import vn.elca.training.validator.ProjectValidator;
 import vn.elca.training.web.AbstractApplicationController;
 
@@ -19,7 +16,7 @@ import vn.elca.training.web.AbstractApplicationController;
  * @author gtn
  */
 @SpringBootApplication(scanBasePackages = "vn.elca.training")
-@ComponentScan(basePackageClasses = {AbstractApplicationController.class, ApplicationMapper.class, ProjectService.class, ProjectValidator.class, ProjectMapper.class})
+@ComponentScan(basePackageClasses = {AbstractApplicationController.class, ProjectService.class, ProjectValidator.class, ProjectMapper.class, GroupMapper.class})
 @PropertySource({"classpath:/application.properties", "classpath:/messages.properties"})
 @EnableJpaRepositories(basePackages = "vn.elca.training.repository")
 public class ApplicationWebConfig extends SpringBootServletInitializer {
@@ -27,10 +24,5 @@ public class ApplicationWebConfig extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(ApplicationWebConfig.class);
-    }
-
-    @Bean
-    public ServletRegistrationBean<WebServlet> h2servletRegistration() {
-        return new ServletRegistrationBean<>(new WebServlet(), "/h2console/*");
     }
 }
